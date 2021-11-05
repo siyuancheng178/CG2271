a65d0f52d8135d28e5bf91ae80a574b344f713de
Siyuan Cheng <siyuanc6794@gmail.com>
2021-11-04 20:37:19 -0700

    audio

--- audio.c
+++ audio.c
@@ -1,42 +1,49 @@
 #include "MKL25z4.h"
 #include "PWM.h"
-#define PTB0_Pin 0
-#define PTB1_Pin 1
-#define MUSICAL_NOTE_CNT 7
+#define PTD2_Pin 2
+#define MUSICAL_NOTE_CNT 42
+#define END_NOTE_CNT 14
+
 #define FREQ_2_MOD(x) (375000 / x)
 
-int musical_notes[] = {256, 256, 384, 384, 427, 427, 384};
-int musical_end[] = {341, 341, 320, 320, 288, 288, 256};
+#define c 261
+#define d 294
+#define e 329
+#define f 349
+#define g 392
+#define a 440
+#define b 493
 
-void initPWM(void)
+int musical_notes[] = {c, c, g, g, a, a, g, f, f, e, e, d, d, c, g, g, f, f, e, e, d, g, g, f, f, e, e, d, c, c, g, g, a, a, g, f, f, e, e, d, d, c};
+int musical_end[] = {c, d, e, c, c, d, e, c, e, f, g, e, f, g};
+
+void iniPWM(void)
 {
-	SIM_SCGC5 |= SIM_SCGC5_PORTB_MASK;
+	SIM_SCGC5 |= SIM_SCGC5_PORTD_MASK;
 	
-	PORTB->PCR[PTB0_Pin] &= ~PORT_PCR_MUX_MASK;
-	PORTB->PCR[PTB0_Pin] |= PORT_PCR_MUX(3);
+	PORTD->PCR[PTD2_Pin] &= ~PORT_PCR_MUX_MASK;
+	PORTD->PCR[PTD2_Pin] |= PORT_PCR_MUX(4);
 	
-	PORTB->PCR[PTB1_Pin] &= ~PORT_PCR_MUX_MASK;
-	PORTB->PCR[PTB1_Pin] |= PORT_PCR_MUX(3);
 	
 	//enable clock gating for timer1
-	SIM->SCGC6 |= SIM_SCGC6_TPM1_MASK;
+	SIM->SCGC6 |= SIM_SCGC6_TPM0_MASK;
 	
 	SIM->SOPT2 &= ~SIM_SOPT2_TPMSRC_MASK;
 	SIM->SOPT2 |= SIM_SOPT2_TPMSRC(1); //mcgfllclk
 	
 	// set module value 48000000 / 128 = 575000 /7500 = 50hz
-	TPM1->MOD = 7500;
+	TPM0->MOD = 7500;
 	
 	
 	//edge aligened pwm
 	//update snc register: cmod=01 ps = 111(128)
-	TPM1->SC &= ~((TPM_SC_CMOD_MASK) | (TPM_SC_PS_MASK));
-	TPM1->SC |= (TPM_SC_CMOD(1) | TPM_SC_PS(7));
-	TPM1->SC &= ~(TPM_SC_CPWMS_MASK);
+	TPM0->SC &= ~((TPM_SC_CMOD_MASK) | (TPM_SC_PS_MASK));
+	TPM0->SC |= (TPM_SC_CMOD(1) | TPM_SC_PS(4));
+	TPM0->SC &= ~(TPM_SC_CPWMS_MASK);
 	
 	//enable pwm on tpm1 channel 0->ptb0
-	TPM1_C0SC &= ~((TPM_CnSC_ELSB_MASK) | (TPM_CnSC_ELSA_MASK) | (TPM_CnSC_MSB_MASK)| (TPM_CnSC_MSA_MASK));
-	TPM1_C0SC |= (TPM_CnSC_ELSB(1) | TPM_CnSC_MSB(1));
+	TPM0_C2SC &= ~((TPM_CnSC_ELSB_MASK) | (TPM_CnSC_ELSA_MASK) | (TPM_CnSC_MSB_MASK)| (TPM_CnSC_MSA_MASK));
+	TPM0_C2SC |= (TPM_CnSC_ELSB(1) | TPM_CnSC_MSB(1));
 	
 }
 
@@ -54,19 +61,32 @@ int main(void)
 {
 	char i=0;
 	SystemCoreClockUpdate();
-	initPWM();
-	TPM1_C0V = 0x0EA6;
+	iniPWM();
+	TPM0_C2V = 0x0EA6;
 	
 	while(1)
 	{
 		for(i=0; i<MUSICAL_NOTE_CNT; i++)
 		{
-			TPM1->MOD = FREQ_2_MOD(musical_notes[i])*2;
-			TPM1_C0V = FREQ_2_MOD(musical_notes[i]);
-			delay(0xFFFF);
+			TPM0->MOD = FREQ_2_MOD(musical_notes[i]);
+			TPM0_C2V = FREQ_2_MOD(musical_notes[i]/2);
+			delay(0xFFFFF);
+			TPM0->MOD = FREQ_2_MOD(0);
+			TPM0_C2V = FREQ_2_MOD(0);
+			delay(0x3FFFF);
 		}
-		TPM1_C0V++;
-		delay(0xFFFF);
+		delay(0xFFFFF);
+		
+		for(i=0; i<END_NOTE_CNT; i++)
+		{
+			TPM0->MOD = FREQ_2_MOD(musical_end[i]);
+			TPM0_C2V = FREQ_2_MOD(musical_end[i]/2);
+			delay(0xFFFFF);
+			TPM0->MOD = FREQ_2_MOD(0);
+			TPM0_C2V = FREQ_2_MOD(0);
+			delay(0x3FFFF);
+		}
+		delay(0xFFFFF);
 	}
 }
 
